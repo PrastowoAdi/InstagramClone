63de44fedef1e7ad276c0c20ba8b838617b9bc25
PrastowoAdi <sinaadi75@gmail.com>
2022-06-04 15:31:22 +0700

    populate data from minifaker and through it inside story component

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
 import Head from "next/head";
+import Feed from "../components/Feed";
 import Header from "../components/Header";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-gray-50 min-h-screen">
       <Head>
         <title>Instagram Clone</title>
       </Head>
       <Header/>
+      <Feed/>
     </div>
   )
 }
