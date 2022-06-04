db748cce5618625db19e3ada70ac3f239aaae053
PrastowoAdi <sinaadi75@gmail.com>
2022-06-04 14:06:38 +0700

    Sticky header component header

--- components/Header.js
+++ components/Header.js
@@ -7,35 +7,36 @@ import { HomeIcon } from '@heroicons/react/solid'
 
 export default function Header() {
   return (
-    <div className='flex items-center justify-between max-w-6xl mx-4 xl:mx-auto'>
-        <div className='cursor-pointer h-24 w-24 relative hidden lg:inline-grid'>
-            <Image
-                src="/img/instagram_logo.svg"
-                layout='fill'
-                className='object-contain'
-            />
-        </div>
-        <div className='cursor-pointer h-24 w-10 relative lg:hidden'>
-            <Image
-                src="/img/instagram_logo_2.svg"
-                layout='fill'
-                className='object-contain'
-            />
-        </div>
-
-        <div className='relative mt-1'>
-            <div className='absolute top-2 left-2'>
-                <SearchIcon className='h-5 text-gray-500'/>
+    <div className='shadow-sm border-b sticky top-0 bg-white z-30'>
+        <div className='flex items-center justify-between max-w-6xl mx-4 xl:mx-auto'>
+            <div className='cursor-pointer h-24 w-24 relative hidden lg:inline-grid'>
+                <Image
+                    src="/img/instagram_logo.svg"
+                    layout='fill'
+                    className='object-contain'
+                />
             </div>
-            <input type="text" placeholder='Search' className='bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md'/>
-        </div>
+            <div className='cursor-pointer h-24 w-10 relative lg:hidden'>
+                <Image
+                    src="/img/instagram_logo_2.svg"
+                    layout='fill'
+                    className='object-contain'
+                />
+            </div>
 
-        <div className='flex space-x-4 items-center'>
-            <HomeIcon className='hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out'/>
-            <PlusCircleIcon className='h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out'/>
-            <img src='/img/avatar-man.png' alt="user-image" className='h-10 rounded-full'/>
+            <div className='relative mt-1'>
+                <div className='absolute top-2 left-2'>
+                    <SearchIcon className='h-5 text-gray-500'/>
+                </div>
+                <input type="text" placeholder='Search' className='bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md'/>
+            </div>
+
+            <div className='flex space-x-4 items-center'>
+                <HomeIcon className='hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out'/>
+                <PlusCircleIcon className='h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out'/>
+                <img src='/img/avatar-man.png' alt="user-image" className='h-10 rounded-full'/>
+            </div>
         </div>
     </div>
-
   )
 }
