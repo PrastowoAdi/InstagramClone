ea4b259b6c9c2023bf597da8543e914ea4ce92a4
PrastowoAdi <sinaadi75@gmail.com>
2022-06-04 14:02:19 +0700

    Add profile image in header component

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,13 @@
+/* eslint-disable @next/next/no-img-element */
 /* eslint-disable jsx-a11y/alt-text */
 import Image from 'next/image'
 import React from 'react'
-import { SearchIcon } from '@heroicons/react/solid'
+import { SearchIcon, PlusCircleIcon } from '@heroicons/react/outline'
+import { HomeIcon } from '@heroicons/react/solid'
 
 export default function Header() {
   return (
-    <div className='flex items-center justify-between max-w-6xl'>
+    <div className='flex items-center justify-between max-w-6xl mx-4 xl:mx-auto'>
         <div className='cursor-pointer h-24 w-24 relative hidden lg:inline-grid'>
             <Image
                 src="/img/instagram_logo.svg"
@@ -28,7 +30,11 @@ export default function Header() {
             <input type="text" placeholder='Search' className='bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md'/>
         </div>
 
-        <h1>Right Side</h1>
+        <div className='flex space-x-4 items-center'>
+            <HomeIcon className='hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out'/>
+            <PlusCircleIcon className='h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out'/>
+            <img src='/img/avatar-man.png' alt="user-image" className='h-10 rounded-full'/>
+        </div>
     </div>
 
   )
